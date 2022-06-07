896112b2e96f7a02c36a9d3e8593eb4125e69076
Endrina Toledo <endrx12@gmail.com>
2022-06-07 19:40:14 -0400

    Video 7: Argumentos

--- platzi_dir/index.js
+++ platzi_dir/index.js
@@ -1,21 +1,21 @@
 'use strict'
 
-const { buildSchema } = require('graphql')
+const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql');
 const { readFileSync } = require('fs')
 const { join } = require('path')
 const resolvers = require('./lib/resolvers')
+
 const app = express()
 const port = process.env.port || 3000
 
 // definiendo el esquema
-const schema = buildSchema(
-  readFileSync(
-    join(__dirname, 'lib', 'schema.graphql'),
-    'utf-8'
-  )
+const typeDefs = readFileSync(
+  join(__dirname, 'lib', 'schema.graphql'),
+  'utf-8'
 )
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 app.use('/api', graphqlHTTP({
   schema: schema,
@@ -24,5 +24,5 @@ app.use('/api', graphqlHTTP({
 }))
 
 app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}/api`);
+  console.log(`Server is listening at http://localhost:${port}/api`)
 })
