cdd79a4006ad924c3bdc33ab68cefab2b3fc546f
Endrina Toledo <endrx12@gmail.com>
2022-06-08 13:25:31 -0400

    Video 9: Integrando una base de datos

--- platzi_dir/lib/db.js
+++ platzi_dir/lib/db.js
@@ -1,26 +1,28 @@
 const { MongoClient } = require('mongodb');
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME,DB_PORT } = process.env;
 
-const mongoUrl = `mongodb+srv://etoledo:<password>@cluster0.wpli2.mongodb.net/test`;
-let connection;
+// const mongoUrl = `mongodb+srv://etoledo:1234@cluster0.wpli2.mongodb.net/test`;
+const mongoUrl = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
+let connection
 
 async function connectDB() {
-  if (connection) return connection;
 
-  let client;
+  if (connection) return connection
+
+  let client
   try {
-    client = new MongoClient(mongoUrl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    connection = await client.connect();
+    client = await MongoClient.connect(mongoUrl, {
+      useNewUrlParser: true
+    })
+    connection = client.db(DB_NAME)
   } catch (error) {
-    console.error('Could not connect to db', mongoUrl, error);
-    process.exit(1);
+    console.error('Could not connect to db', mongoUrl, error)
+    process.exit(1)
   }
 
-  return connection;
+  return connection
+
 }
 
 module.exports = connectDB;
